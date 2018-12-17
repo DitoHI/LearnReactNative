202db1f2cb9a34b81e3b6906ffab9245f288e471
SInichiDito <ditohafizh@gmail.com>
2018-12-17 21:49:03 +0700

    reformat tutorial

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import LoggedOut from './src/screens/LoggedOut';
+
+export default class App extends Component<Props> {
+    render() {
+        return <LoggedOut/>
+    }
+}
